95db4af8ce35c56002a5ae2479d35697f0a5027a
carlosAAF25 <ca34922@gmail.com>
2025-03-27 13:42:48 -0400

    Agregar la tabla Componente a la base de datos;crear endpoints para los componentes;

---
+++ backend/IMT-lab-server/IMT-lab-server/Data/Migrations/20250327115959_AddComponentTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IMT_lab_server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddComponentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Component",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Component", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Component");
+        }
+    }
+}
