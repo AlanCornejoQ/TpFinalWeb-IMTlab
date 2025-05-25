dc6e37db957f9b15c325e1b8550dff4df4bff29f
AlanCornejoQ <ecornejo@ucb.edu.bo>
2025-05-25 19:02:52 -0400

    Entities

---
+++ backend/IMT-lab-server/IMT-lab-server/core/Domain/Entities/Usuario.cs
@@ -0,0 +1,9 @@
+namespace Core.Domain.Entities
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+    }
+}
